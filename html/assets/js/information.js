6a098f6320c16fcbfaabce23e281e5785475d4ac
lythaiho.95.cscd@gmail.com <hothai@95>
2019-08-31 13:51:38 +0700

    update api map

--- html/assets/js/information.js
+++ html/assets/js/information.js
@@ -1,13 +1,13 @@
 function initialize() {
     var map = new google.maps.Map(
         document.getElementById('map'), {
-            center: new google.maps.LatLng(21.028998, 105.781704),
+            center: new google.maps.LatLng(21.028800, 105.782280),
             zoom: 14,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         });
 
     var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(21.028998, 105.781704),
+        position: new google.maps.LatLng(21.028800, 105.782280),
         map: map
     });
 
