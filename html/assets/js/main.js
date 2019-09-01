780229e014f2f4f0e103b17b9ea2db193ae864a4
nanachi1 <dinhvu2509@gmail.com>
2019-09-01 22:21:39 +0700

    fix stufff

---
+++ html/assets/js/main.js
@@ -0,0 +1,10 @@
+(function ($) {
+    "use strict";
+
+    // $('.has_dropdown').hover(function () {
+    //     $(this).find('.dropdowns').slideDown();
+    // }, function () {
+    //     $(this).find('.dropdowns').slideUp();
+    // });
+
+})(jQuery);
