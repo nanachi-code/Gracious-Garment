a5fef91fc8e6df1f7435f68b018cc64b50750c17
nanachi1 <dinhvu2509@gmail.com>
2019-09-08 02:37:40 +0700

    update js

--- html/assets/js/main.js
+++ html/assets/js/main.js
@@ -1,7 +1,9 @@
 (function ($) {
     "use strict";
     let cartObject = null; //* global pointer to cart   
-    
+    let cartOptions = null //* global pointer to cart options
+
+
     //* localStorage function
     function getLocalData(name) {
         return localStorage.getItem(name)
@@ -18,27 +20,51 @@
     function initCart() {
         //* Check in browser memory if there are a saved cart
         if (getLocalData('graciousCart') != null) {
-            //* if previous cart is present load it
+            //* if previous cart is present then load it
             cartObject = JSON.parse(getLocalData('graciousCart'));
+        }
+        if (getLocalData('graciousCartOptions') != null) {
+            //* if previous cart options are present then load it
+            cartOptions = JSON.parse(getLocalData('graciousCartOptions'));
         }
     }
 
-    $("#checkbox").change(function () {
-        if (this.checked) {
-            $("#firstname1").val($("#firstname").val());
-            $("#lastname1").val($("#lastname").val());
-            $("#streetname1").val($("#streetname").val());
-            $("#city1").val($("#city").val());
-            $("#country1").val($("#country").val());
-            $("#phone1").val($("#phone").val());
-        } else {
-            $("#firstname1").val("");
-            $("#lastname1").val("");
-            $("#streetname1").val("");
-            $("#city1").val("");
-            $("#country1").val("");
-            $("#phone1").val("");
+    $('#address-form').submit(function () {
+        cartOptions = {
+            'billing': {
+                'billingName': $("#billingName").val(),
+                'billingAddress': $("#billingAddress").val(),
+                'billingPhone': $("#billingPhone").val(),
+            },
+
+            'delivery': {
+                'deliveryName': $("#deliveryName").val(),
+                'deliveryAddress': $("#deliveryAddress").val(),
+                'deliveryPhone': $("#deliveryPhone").val(),
+            },
+            'comment': $("#comment").val(),
+            'shipping': '',
+            'payment': ''
         }
+        setLocalData('graciousCartOptions', JSON.stringify(cartOptions));
+    });
+
+    $("#copyAddress").change(function () {
+        if (this.checked) {
+            $("#deliveryName").val($("#billingName").val());
+            $("#deliveryAddress").val($("#billingAddress").val());
+            $("#deliveryPhone").val($("#billingPhone").val());
+        } else {
+            $("#deliveryName").val("");
+            $("#deliveryAddress").val("");
+            $("#deliveryPhone").val("");
+        }
+    });
+
+    $('.remove-cart').click(function (e) {
+        e.preventDefault();
+        removeFromCart($(this).attr('data-id'));
+        $(this).parent().parent().parent().parent().remove();
     });
 
     //* Load header cart
@@ -91,28 +117,6 @@
         }
     }
 
-    //* Add to cart
-    function addToCart(id, name, brand, size, color, price, quantity, imgURL) {
-        //* Check if this item is already in the cart or not
-        cartObject.forEach(element => {
-            //* if it is already in the cart then increase the quantity
-            if (element.id == id) {
-                element.quantity = element.quantity + quantity;
-            }
-        });
-        //* if not then add new item to localStorage
-        let cartItem = {
-            'id': id,
-            'name': name,
-            'brand': brand,
-            'price': price,
-            'imgURL': imgURL,
-            'quantity': quantity
-        };
-        cartObject.push(cartItem);
-        setLocalData('graciousCart', JSON.stringify(cartObject));
-    }
-
     //* Load cart page
     function loadPageCart() {
         if (cartObject != null) {
@@ -134,9 +138,14 @@
                             <input type="number" class="form-control text-26 col-md-5 text-bold" value="${element.quantity}">
                         </div>
                         <div class="col-md-2 align-self-center">
-                            <div>
-                                <h5 class="line price-tag text-secondary">${element.quantity}x/$${element.price}</h5>
-                                <h1 class="price-tag text-bold">/$${singleTotalPrice}</h1>
+                            <div class="row>
+                                <div class="col-md-9">
+                                    <h5 class="line price-tag text-secondary">${element.quantity}x/$${element.price}</h5>
+                                    <h1 class="price-tag text-bold">/$${singleTotalPrice}</h1>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="remove-cart" data-id="${element.id}">x</div>
+                                </div>
                             </div>
                         </div>
                     </div>`
@@ -160,7 +169,7 @@
         }
     }
 
-    //* load summary page
+    //* Load summary page
     function loadPageSummary() {
         if (cartObject != null) {
             //* if cartObject is set then display the header cart
@@ -207,6 +216,38 @@
         }
     }
 
+    //* Cart methods
+    //* Add to cart
+    function addToCart(id, name, brand, price, quantity, imgURL) {
+        //* Check if this item is already in the cart or not
+        cartObject.forEach(element => {
+            //* if it is already in the cart then increase the quantity
+            if (element.id == id) {
+                element.quantity = element.quantity + quantity;
+            }
+        });
+        //* if not then add new item to localStorage
+        let cartItem = {
+            'id': id,
+            'name': name,
+            'brand': brand,
+            'price': price,
+            'imgURL': imgURL,
+            'quantity': quantity
+        };
+        cartObject.push(cartItem);
+        setLocalData('graciousCart', JSON.stringify(cartObject));
+    }
+
+    //* Remove from cart
+    function removeFromCart(id) {
+        let afterFilter = cartObject.filter((element) => {
+            element.id != id;
+        })
+
+        cartObject = afterFilter;
+        setLocalData('graciousCart', JSON.stringify(cartObject));
+    }
 
     $(document).ready(function () {
         initCart();
