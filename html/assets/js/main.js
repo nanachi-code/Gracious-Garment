eb6b0c202dc3dea6d70e645ecf10290b8dbc1797
lythaiho.95.cscd@gmail.com <hothai@95>
2019-09-20 15:55:12 +0700

    add map and logo

--- html/assets/js/main.js
+++ html/assets/js/main.js
@@ -361,7 +361,7 @@
             return false;
         }
     });
-    
+
     //* Admin page
     //* Permalink generator
     $('#productName').change(function () {
@@ -405,3 +405,26 @@
 
 
 })(jQuery);
+
+// Map Api
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 21.028765, lng: 105.781707},
+        zoom: 16
+    });
+    marker = new google.maps.Marker({
+        map: map,
+        draggable: true,
+        animation: google.maps.Animation.DROP,
+        position: {lat: 21.028765, lng: 105.781707}
+    });
+    marker.addListener('click', toggleBounce);
+}
+function toggleBounce() {
+    if (marker.getAnimation() !== null) {
+        marker.setAnimation(null);
+    } else {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+}
